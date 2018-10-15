7fc44249d3bf15814e0fff0be52e3b20cbc74d4a
Sotiria <s.k.patroni@gmail.com>
2018-10-15 22:39:29 +0100

    create test for setOffers in DatabaseManager test

--- src/my/demo/eshop/test/DatabaseManagerTest.java
+++ src/my/demo/eshop/test/DatabaseManagerTest.java
@@ -51,4 +51,37 @@ public class DatabaseManagerTest  extends TestCase {
 			} catch (SQLException e){}
 		}
 	}
+	
+	public void testSetOffer(){
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.S");
+		
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn  = DriverManager.getConnection("jdbc:mysql://localhost:3306/demoeshop","root","sotiria");
+			
+			databaseManager.setOffers(conn, new Offer(2, "offer2", "simple description", "200", formatter.parseDateTime("2018-10-15 22:21:11.0"));
+			
+			ArrayList<Offer> actualOffers = databaseManager.getOffers(conn);
+			
+			//for the sake of simplicity I check only the first object in the ArrayList
+			assertEquals(actualOffers.get(1).getOfferId(), 2);
+			assertEquals(actualOffers.get(1).getDescription(), "simple description");
+			assertEquals(actualOffers.get(1).getName(), "offer2");
+			assertEquals(actualOffers.get(1).getPrice(), "200");
+			assertEquals(actualOffers.get(1).getExpirationDate().toString(), "2018-10-15 22:21:11.0");
+		}
+		catch (SQLException e)
+		{
+			throw new RuntimeException("SQL error : " + e.getMessage());
+		} catch (Exception e) {
+			throw new RuntimeException("Exception in generating the report : " + e.getMessage());
+		}
+		finally 
+		{
+			try 
+			{	if(conn!=null){conn.close();}	
+			} catch (SQLException e){}
+		}
+	}
 }
